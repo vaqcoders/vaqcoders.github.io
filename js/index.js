2ae3901127d50fb33fbac57c11dcb6f9982cf96e
Ethan <ethan.r.davidson@gmail.com>
2018-09-20 00:18:48 -0700

    Attempted an responsive design solution with a toggleMobile function

--- js/index.js
+++ js/index.js
@@ -11,6 +11,16 @@ function anchor(id) {
   });
 }
 
+function toggleMobile() {
+  const isMobile = window.navigator.userAgent.toLowerCase().includes("mobile");
+  const scl = isMobile ? 3.25 : 1;
+  document.querySelectorAll("h1, h2, h3, h4, p").forEach(el => {
+    const temp = Number(el.style.fontSize) || 1;
+    const fontSize = isMobile ? temp * scl : temp / scl;
+    el.style.fontSize = `${fontSize}em`;
+  });
+}
+
 // Visit https://p5js.org/reference/ for extra info and for discovering other functions to use. If you have any questions, please do not hesitate to ask the Discord server.
 // If things begin to seem limited on codepen and Khan Academy, I invite you to visit https://editor.p5js.org/ for a different experience.
 // Keep in mind that the setup function and draw function must both be declared in the js file in order to use the p5 library as intended.
